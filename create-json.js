336e2f79dff9f2a76d2fa0bdd360f7b16a46d983
João Gabriel de Almeida <110470218+cocacolacomvodka@users.noreply.github.com>
2023-08-26 14:02:44 -0300

    Update create-json.js

--- create-json.js
+++ create-json.js
@@ -1,7 +1,9 @@
 const fs = require("fs").promises;
 
+const { Octokit } = require("octokit");
+
 async function generateJSON() {
-  try {
+    try {
     const portfolioFiles = await fs.readdir("portfolios");
 
     const portfolios = await Promise.all(portfolioFiles.map(async (filename) => {
@@ -16,6 +18,19 @@ async function generateJSON() {
   } catch (error) {
     console.error("Error generating JSON:", error);
   }
+
+  // Usar o Octokit para criar/atualizar o arquivo no repositório
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+  await octokit.request('PUT /repos/:owner/:repo/contents/:path', {
+    owner: 'cocacolacomvodka',
+    repo: 'Git-portfolio-markdown',
+    path: 'portfolios.json',
+    message: 'Update portfolios.json',
+    content: Buffer.from(JSON.stringify(portfolios, null, 2)).toString('base64'),
+    sha: existingShaOfPortfoliosJson // Obtenha o SHA atual do arquivo para atualização
+  });
 }
 
 generateJSON();
+
