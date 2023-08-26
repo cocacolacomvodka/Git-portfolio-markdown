58c9daef18f431babf7133a7690c8cdfa759585b
João Gabriel de Almeida <110470218+cocacolacomvodka@users.noreply.github.com>
2023-08-26 14:20:56 -0300

    Update create-json.js

--- create-json.js
+++ create-json.js
@@ -1,36 +1,34 @@
 const fs = require("fs").promises;
-
 const { Octokit } = require("octokit");
 
 async function generateJSON() {
-    try {
-    const portfolioFiles = await fs.readdir("portfolios");
+  try {
+    const octokit = new Octokit({ auth: secrets.GIT_TOKEN });
 
-    const portfolios = await Promise.all(portfolioFiles.map(async (filename) => {
-      const fileContent = await fs.readFile(`portfolios/${filename}`, "utf-8");
-      const [title, description] = fileContent.split("\n");
+    // Use a API do GitHub para obter as informações do arquivo portfolios.json
+    const response = await octokit.request('GET /repos/:owner/:repo/contents/:path', {
+      owner: 'cocacolacomvodka',
+      repo: 'Git-portfolio-markdown',
+      path: 'portfolios.json'
+    });
 
-      return { title, description, fileContent };
-    }));
+    const existingShaOfPortfoliosJson = response.data.sha; // Obtenha o SHA do último commit
 
-    await fs.writeFile("portfolios.json", JSON.stringify(portfolios, null, 2));
-    console.log("JSON file created successfully!");
+    // ... o restante do seu código para gerar o arquivo JSON
+
+    await octokit.request('PUT /repos/:owner/:repo/contents/:path', {
+      owner: 'cocacolacomvodka',
+      repo: 'Git-portfolio-markdown',
+      path: 'portfolios.json',
+      message: 'Update portfolios.json',
+      content: Buffer.from(JSON.stringify(portfolios, null, 2)).toString('base64'),
+      sha: existingShaOfPortfoliosJson // Use o SHA obtido
+    });
+
+    console.log("JSON file updated successfully!");
   } catch (error) {
     console.error("Error generating JSON:", error);
   }
-
-  // Usar o Octokit para criar/atualizar o arquivo no repositório
-  const octokit = new Octokit({ auth: process.env.GIT_TOKEN });
-
-  await octokit.request('PUT /repos/:owner/:repo/contents/:path', {
-    owner: 'cocacolacomvodka',
-    repo: 'Git-portfolio-markdown',
-    path: 'portfolios.json',
-    message: 'Update portfolios.json',
-    content: Buffer.from(JSON.stringify(portfolios, null, 2)).toString('base64'),
-    sha: existingShaOfPortfoliosJson // Obtenha o SHA atual do arquivo para atualização
-  });
 }
 
 generateJSON();
-
